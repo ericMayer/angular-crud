e7b36ce062e9dd5cbf594583383eab7404896e37
ericMayer <eric.araujo_mayer@hotmail.com>
2020-07-13 14:41:58 -0300

    Criado componente do conteúdo

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,14 +4,15 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HeaderComponent } from "./components/template/header/header.component";
-
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { FooterComponent } from "./components/template/footer/footer.component";
-import { NavegacaoComponent } from "./components/template/navegacao/navegacao.component";
-
 import { MatSidenavModule as SideNav } from "@angular/material/sidenav";
 import { MatListModule as List } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+
+import { HeaderComponent } from "./components/template/header/header.component";
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { NavegacaoComponent } from "./components/template/navegacao/navegacao.component";
+import { ContentComponent } from "./views/content/content.component";
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { MatListModule as List } from "@angular/material/list";
     HeaderComponent,
     FooterComponent,
     NavegacaoComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,6 +29,7 @@ import { MatListModule as List } from "@angular/material/list";
     MatToolbarModule,
     SideNav,
     List,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
